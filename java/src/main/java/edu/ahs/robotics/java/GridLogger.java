3695849c97dc35125d04a2b016f9000fa0359035
BeauT12235 <67128469+BeauT12235@users.noreply.github.com>
2020-07-14 15:13:36 -0600

    into problem set 4

---
+++ java/src/main/java/edu/ahs/robotics/java/GridLogger.java
@@ -0,0 +1,80 @@
+package edu.ahs.robotics.java;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class GridLogger {
+    private boolean firstRow = true;
+
+    private HashSet<String> categorySet;
+    private ArrayList<String> categories = new ArrayList<>();
+    private HashMap<String, String> rowValues;
+
+    private LogWriter writer;
+
+    public GridLogger(LogWriter writer) {
+        this.writer = writer;
+        categorySet = new HashSet<>();
+        rowValues = new HashMap<>();
+
+    }
+
+    /**
+     * Add a value to the logger under the category.  Categories are lazily added to the logger
+     * in the order encountered.
+     *
+     * @param category
+     * @param value
+     */
+    public void add(String category, String value) {
+        if (firstRow&&!categorySet.contains(category)) {
+            //We are adding the category to the HashSet and ArrayList.
+            categories.add(category);
+            categorySet.add(category);
+
+        }
+        rowValues.put(category, value);
+    }
+
+    /**
+     * Write a line of data to the log.  If this is the first call to writeLn, categories are
+     * written first, followed by the line of data.  Once the data is written, the logger is reset
+     * and calls to add() will add values to the next line of data.
+     */
+    public void writeLn() {
+        if (firstRow) {
+            StringBuffer header = new StringBuffer();
+            for (int i = 0; i < categories.size(); i++) {
+                header.append(categories.get(i));
+                if (i < categories.size() - 1) {
+                    header.append(",");
+                }
+            }
+            writer.writeLine(header.toString());
+            firstRow = false;
+        }
+
+        StringBuffer valueRow = new StringBuffer();
+        for (int i = 0; i < categories.size(); i++) {
+            String category = categories.get(i);
+            String value = rowValues.get(category);
+            if (value!=null){
+                valueRow.append(value);
+            }
+            if (i < categories.size() - 1) {
+                valueRow.append(",");
+            }
+        }
+        writer.writeLine(valueRow.toString());
+        rowValues.clear();
+    }
+
+    public void stop() {
+    }
+}
+
+
+
+
